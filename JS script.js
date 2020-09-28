faa207f8dddba90719c9e638075fe3ea467d2ca7
Your Name <nntwari75@gmail.com>
2020-09-28 02:47:25 -0700

    some random message

---
+++ JS script.js
@@ -0,0 +1,27 @@
+var dd=birthday
+var mm=birthmonth
+var yy=(birthyear/100-Math.floor birthyear/100)*100
+var birthcentury=cc
+var cc= birthyear%100===0
+var cc=true;
+if (cc){console.log(birthyear/100)} else {
+  console.log(Math.floor birthyear/100 + 1);
+}
+
+
+
+var calculate=function(dd,mm,yy,cc){return( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7}
+
+var result =calculate(dd,mm,yy,cc)
+if(result=1{console.log ("your akan name is kwadwo");})else if(result=2{console.log("your akan name is kwabena");})
+else if (result=3){console.log(your akan name is kwaku); }else if (result=4) {console.log(your akan name is yaw);}
+else if (result=5) {console.log(your akan name is kofi);}else if (result=6) {console.log(your akan name is kwame);}
+else if(result=7){console.log(your akan name is kwasi);}
+
+}
+
+}
+
+}
+
+}
