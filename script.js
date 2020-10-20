5e05801d3a893d4fcefcf00527cf61f82e0d668b
Your Name <nntwari75@gmail.com>
2020-10-20 01:05:13 -0700

    random messages

---
+++ script.js
@@ -0,0 +1,31 @@
+var dd=birthday
+var mm=birthmonth
+var yy=(birthyear/100-Math.floor birthyear/100)*100
+var birthcentury=cc
+var cc= birthyear%100===0
+var cc=true;
+if (cc){console.log(birthyear/100)} else {
+  console.log(Math.floor birthyear/100 + 1);
+}
+
+
+
+function calculate(dd,mm,yy,cc){return( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7}
+var male = [kwadwo,kwabena,kwaku,yaw,kofi,kwame,kwasi]
+var female=[adwoa,adwoa,akua,yaa,afua,ama,akosua]
+var result =calculate(dd,mm,yy,cc)
+if( male===true && result=1{alert (male[0]);})else if(male===true &&result=2{alert(male[1]);})
+else if (male===true &&result=3){alert(male[2]); }else if (male===true &&result=4){alert(male[3]);}
+else if (male===true&&result=5) {alert(male[4]);}else if (male===true&&result=6) {alert(male[5]);}
+else if(male===true&&result=7){alert(male[6]);}else if( male===false && result=1{alert (female[0]);})else if(male===false &&result=2{alert(female[1]);})
+else if (male===false &&result=3){alert(female[]2); }else if (male===false &&result=4) {alert(female[3]);}
+else if (male===false&&result=5) {alert(female[4]);}else if (male===false&&result=6) {alert(female[5]);}
+else if(male===false&&result=7){alert(female[6]);}
+
+}
+
+}
+
+}
+
+}
